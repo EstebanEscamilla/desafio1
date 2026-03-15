56c489956df59b0bb773455b00aabba698fcefe0
santiagoLoaiza10 <castanoloaizasantiago@gmail.com>
2026-03-15 13:58:42 -0500

    Corrección de máscaras, generación de piezas y Rotaciones:
    Se corrigieron algunas máscaras para que aparecieran en la posición original del teclado.
    Se creó la función que escoge una pieza (en la posición natural) de forma aleatoria.
    Comentarios a todas las funciones y estructuras de datos.
    Se tuvo que agregar la librería "ctime" para darle una semilla diferente al rand cada segundo.
    
    Siguiente paso:
    Crear el tablero

--- Sol_desafio1/main.cpp
+++ Sol_desafio1/main.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
-#include "validaciones.h"
+#include <cstdint> // Esta es para los unitx_t
+#include <ctime> // Para generar la semilla (Número aleatorio)
+
+#include "validaciones.h" // Modulo para validar entradas
+#include "piezas.h" // Modulo para generar piezas y sus máscaras
 
 using namespace std;
 
 int main(){
     int ancho = 0, alto = 0, *ptr_ancho = NULL, *ptr_alto = NULL;
+    int pieza_actual = 0 , *dir_pieza_actual = NULL;
+    uint16_t mascara_actual = 0;
+
+    srand(time(NULL)); // Para generar una semilla con los segundos del reloj de la hora actual
 
     cout << "=== BIEVENIDO A TETRIS ===" << endl;
 
     cout << "Ingrese el ancho del tablero: "; cin >> ancho;
     cout << "Ingrese el alto del tablero: "; cin >> alto;
 
+    //Asignamos todos los punteros a sus variables correspondientes
     ptr_ancho = &ancho; ptr_alto = &alto;
+    dir_pieza_actual = &pieza_actual;
 
     // Verificamos que las dimensiones del tablero sean correctas
     VerificarDimensiones(ptr_ancho, ptr_alto);
 
-    // Una vez se genere la pieza debemos verificar que sea una de las permitidas
-
+    // Ahora generamos nuestra pieza de forma aleatoria (en su posición natural)
+    mascara_actual = GenerarPiezaAleatoria(dir_pieza_actual);
 
     return 0;
 }
