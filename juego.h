be1f36a463cd280f5bb5b857cd0e7cd5cc6d0c42
EstebanEscamilla <esteban.escamilla@udea.edu.co>
2026-03-19 22:55:47 -0500

    limpieza de carpeta
    Limpieza para descargar la funcionalidad completa y realizar la documentacion y comentario de las funciones

--- juego.h
+++
@@ -1,8 +0,0 @@
-#ifndef JUEGO_H
-#define JUEGO_H
-
-#include <cstdint>
-
-void IniciarJuego(uint8_t **tablero, int ancho, int alto);
-
-#endif // JUEGO_H
--- piezas.h
+++
@@ -1,7 +0,0 @@
-#ifndef PIEZAS_H
-#define PIEZAS_H
-#include <cstdint>
-
-uint16_t GenerarPiezaAleatoria(int *dir_pieza_actual);
-
-#endif // PIEZAS_H
