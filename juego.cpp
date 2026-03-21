77e8fc90ceb972a163b026d1b2fd2786cffcb3cf
EstebanEscamilla <esteban.escamilla@udea.edu.co>
2026-03-21 18:46:59 -0500

    docstring de LimpiarLineas y IniciarJuego
    Se hace los comentarios y docstring de estas 2 funciones. Listo para integrar en el modulo de  backend y frontend en un futuro.

--- juego.cpp
+++ juego.cpp
@@ -7,49 +7,88 @@
 using namespace std;
 
 void LimpiarLineas(uint8_t **tablero, int ancho, int alto) {
+
+    /*
+    Recibe: El tablero (Puntero a punteros), ancho y alto
+    Hace:Recorre las filas de abajo hacia arriba. Si encuentra una fila donde todos los bits de
+    todos los bytes son 1 (0xFF), la elimina: mueve los punteros de las filas superiores hacia
+    abajo, limpia la memoria de la fila eliminada y la coloca como nueva fila superior.
+    Retorna: El tablero sin filas llenas
+    */
+
+    //Calcular los bytes que ocupa cada fila.
     int BytesPorFila = ancho / 8;
 
+    // Recorrer las filas desde la mas abajo a la mas arriba.
     for (int i = alto - 1; i >= 0; i--) {
 
+        //Asumir que la fila esta llena.
         bool fila_llena = true;
 
+        //Recorremos byte por fila.
         for (int j = 0; j < BytesPorFila; j++) {
 
+            // Si ese byte esta lleno no esta lleno (0xFF) convertimos la variable fila_llena en false y rompemos.
             if (tablero[i][j] != 255) {
                 fila_llena = false;
                 break;
             }
         }
 
+        // Evaluamos  si la fila esta llena se procede a eliminarla.
         if (fila_llena) {
 
+            //guardamos el puntero de la fila que vamos a eliminar porque después
+            //de mover las filas, ese puntero seguirá apuntando a la memoria de la fila original.
+            //La usaremos como "cajón vacío" para la nueva fila superior.
             uint8_t *CajonVacio = tablero[i];
 
+            //desplaza los punteros de las filas hacia abajo.
             for (int k = i; k > 0; k--) {
                 tablero[k] = tablero[k - 1];
             }
 
+            //limpia la memoria que antes era la fila eliminada, llenándola de ceros.
             for (int j = 0; j < BytesPorFila; j++) {
                 CajonVacio[j] = 0;
             }
 
+            //coloca ese bloque de memoria vacío en la posición superior (fila 0). Así, el tablero
+            //sigue teniendo alto filas, pero ahora una fila vacía está arriba.
             tablero[0] = CajonVacio;
+
+            // después de eliminar una fila, se incrementa i para que el bucle for
+            //vuelva a examinar la misma posición esto permite verificar la nueva fila
+            // que bajó, porque puede estar llena tambien.
             i++;
 
+            //Mensaje de confirmacion.
             cout << "¡LINEA COMPLETADA Y LIMPIADA!" << endl;
         }
     }
 }
 
 void IniciarJuego(uint8_t **tablero, int ancho, int alto){
+
+    /*
+    Recibe:El tablero, el ancho y el alto.
+    Hace:Maneja la entrada del usuario, actualiza la posición y rotación de la pieza,
+    detecta colisiones, fija piezas, limpia filas y finaliza el juego (GAME OVER).
+    Retorna: flujo infinito del juego hasta que tecla q se aprete o haya un game over
+    */
+
+    // Definicion de variables usadas durante el juego.
     int PosX = 0, PosY = 0, Bandera_JuegoActivo = 1;
     int pieza_actual = 0 , *dir_pieza_actual = NULL; // Piezas
     uint16_t mascara_actual = 0; // Máscara de bits en cada turno
     char tecla;
     int rotacion_actual = 0;
 
+    //Establece el puntero para que generarpiezaaleatoria pueda modificar pieza_actual
     dir_pieza_actual = &pieza_actual;
+    //Centra la pieza en la parte superior
     PosX = (ancho / 2) - 2;
+    //Colocar la pieza 3 filas por arriba del tablero.
     PosY = -3;
 
     // Ahora generamos nuestra pieza de forma aleatoria (en su posición natural)
@@ -59,17 +98,19 @@ void IniciarJuego(uint8_t **tablero, int ancho, int alto){
 
     while(Bandera_JuegoActivo){
 
-        // Imprimimos el tablero antes de que se ingrese la pieza
+        // Imprimimos el tablero antes de que se ingrese la pieza.
         ImprimirTablero(tablero, ancho, alto, mascara_actual, PosX, PosY);
 
         // Capturamos la letra ingresada
         tecla = CapturarAccion();
 
+        //Se guardan posibles nuevas posiciones, rotación y máscara, inicialmente iguales a las actuales.
         int NuevaPosX = PosX;
         int NuevaPosY = PosY;
         int NuevaRotacion = rotacion_actual;
         uint16_t NuevaMascara = mascara_actual;
 
+        //Según la tecla, se modifican los valores temporales.
         if (tecla == 'a' || tecla == 'A') {
             NuevaPosX--;
         }
@@ -87,10 +128,13 @@ void IniciarJuego(uint8_t **tablero, int ancho, int alto){
             Bandera_JuegoActivo = 0;
         }
 
+        // Si la tecla no es Q o q se verifica la colisión.
         if (tecla != 'q' && tecla != 'Q') {
 
+            //Colision
             int choque = HayColision(tablero, ancho, alto, NuevaMascara, NuevaPosX, NuevaPosY);
 
+            //Si no hay colision choque sera igual a 0
             if (choque == 0) {
                 PosX = NuevaPosX;
                 PosY = NuevaPosY;
@@ -98,11 +142,14 @@ void IniciarJuego(uint8_t **tablero, int ancho, int alto){
                 mascara_actual = NuevaMascara;
             }
             else {
-
+                // Si hay colision y la tecla era s es que ya no puede bajar mas.
                 if (tecla == 's' || tecla == 'S') {
 
+                    //Se funde la pieza en el tablero.
                     FundirPieza(tablero, mascara_actual, PosX, PosY);
+                    //Se limpian las filas que apliquen.
                     LimpiarLineas(tablero, ancho, alto);
+                    //La pieza se fijo cuando aun estaba parcialmnete afuera
                     if (PosY < 0) {
 
                         ImprimirTablero(tablero, ancho, alto, mascara_actual, PosX, PosY);
@@ -114,11 +161,14 @@ void IniciarJuego(uint8_t **tablero, int ancho, int alto){
 
                         Bandera_JuegoActivo = 0; // Esto es para que se salga del while
                     }
+                    //Si no se genera una nueva pieza en la parte superior
                     else {
                         PosX = (ancho / 2) - 2;
                         PosY = -3;
                         rotacion_actual = 0;
                         mascara_actual = GenerarPiezaAleatoria(dir_pieza_actual);
+
+                    // El juego continua hasta que el jugador salga o termine el juego.
                     }
                 }
             }
